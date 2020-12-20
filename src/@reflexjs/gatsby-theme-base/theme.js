75b468521f0bca40c1a9292d5e999bb113b7dd1a
Ruixiang Du <ruixiang.du@gmail.com>
2020-12-20 21:52:09 +0800

    updated dark theme color

--- src/@reflexjs/gatsby-theme-base/theme.js
+++ src/@reflexjs/gatsby-theme-base/theme.js
@@ -5,5 +5,22 @@ export default {
 
   colors: {
     primary: `#005ae0`,
+    modes: {
+        dark: {
+          text: "#fff",
+          background: "#191924",
+          heading: "#fff",
+          primary: "#e67235",
+          primaryHover: "#d22d16",
+          secondary: "#07070a",
+          secondaryHover: "#191924",
+          accent: "#fb3640",
+          accentHover: "#fa0f1b",
+          muted: "#2a2a3c",
+          mutedHover: "#212130",
+          border: "#2a2a3c",
+          borderHover: "#434360",
+        },
+      },
   },
 }
