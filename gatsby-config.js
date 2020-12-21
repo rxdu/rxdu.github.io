a3ec247fd29f06583f9705d859274dc3ee5b4942
Ruixiang Du <ruixiang.du@gmail.com>
2020-12-21 22:07:25 +0800

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,18 @@ module.exports = {
         },
     },
     {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: `gatsby-starter-default`,
+          short_name: `starter`,
+          start_url: `/`,
+          background_color: `#663399`,
+          theme_color: `#663399`,
+          display: `minimal-ui`,
+          icon: `./content/images/favicon.png`, // This path is relative to the root of the site.
+        },
+    },
+    {
         resolve: `gatsby-plugin-google-analytics`,
         options: {
           // The property ID; the tracking code won't be generated without it
