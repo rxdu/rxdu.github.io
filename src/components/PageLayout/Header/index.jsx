c2c47da407ae3eed6b2d15d0f861d0d9d5581083
Ruixiang Du <ruixiang.du@gmail.com>
2023-02-05 22:36:12 +0800

    notes: redirect to new notes site

--- src/components/PageLayout/Header/index.jsx
+++ src/components/PageLayout/Header/index.jsx
@@ -47,7 +47,7 @@ export default () => {
               </Link>
             </li>
             <li className={style.navItem}>
-              <Link to="https://note.rdu.im" onClick={toggleMenu} activeClassName={style.anchorActive}>
+              <Link to="https://notes.rdu.im" onClick={toggleMenu} activeClassName={style.anchorActive}>
                 Notes
               </Link>
             </li>
